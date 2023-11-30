8f750347c7e4e742cd122b6521dd7ad4621fd5d5
LuK1337 <priv.luk@gmail.com>
2023-11-30 20:47:49 +0100

    fajita: Add firmware
    Matches blobs in the device tree.
    
    Change-Id: I026549a072cdb73a01d2abb3fbe3c32e06582f3b

--- Android.mk
+++ Android.mk
@@ -6,4 +6,23 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),fajita)
 
+$(call add-radio-file,radio/LOGO.img)
+$(call add-radio-file,radio/abl.img)
+$(call add-radio-file,radio/aop.img)
+$(call add-radio-file,radio/bluetooth.img)
+$(call add-radio-file,radio/cmnlib.img)
+$(call add-radio-file,radio/cmnlib64.img)
+$(call add-radio-file,radio/devcfg.img)
+$(call add-radio-file,radio/dsp.img)
+$(call add-radio-file,radio/fw_4j1ed.img)
+$(call add-radio-file,radio/fw_4u1ea.img)
+$(call add-radio-file,radio/hyp.img)
+$(call add-radio-file,radio/keymaster.img)
+$(call add-radio-file,radio/modem.img)
+$(call add-radio-file,radio/qupfw.img)
+$(call add-radio-file,radio/storsec.img)
+$(call add-radio-file,radio/tz.img)
+$(call add-radio-file,radio/xbl.img)
+$(call add-radio-file,radio/xbl_config.img)
+
 endif
